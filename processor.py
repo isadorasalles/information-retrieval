63f5027576804e3a401fb3bc26fb6a5d07571060
isadorasalles <isadorasalles@dcc.ufmg.br>
2022-06-15 16:43:15 -0300

    daat mais rapido

--- processor.py
+++ processor.py
@@ -9,14 +9,14 @@ import json
 import math
 import time
 from concurrent.futures import ThreadPoolExecutor
-from collections import defaultdict
+from collections import Counter
 
 stopwords = nltk.corpus.stopwords.words('portuguese')
 next_query = Queue()
 count_queries_processed = 0
 mutex_count_queries = Lock()
 
-MAX_THREADS = 50
+MAX_THREADS = 5
 num_threads = 0
 mutex_count_threads = Lock()
 
@@ -33,31 +33,21 @@ def check_end_query_processing():
         return False
     mutex_count_queries.release()
     return True
-    
 
 def preprocess(text):
-    remove_digits = str.maketrans('', '', digits)
-    text_no_numbers = text.translate(remove_digits)
-    tokens = nltk.word_tokenize(text_no_numbers.lower()) # lowercase
-    tokens = [token.strip(punctuation) for token in tokens if token not in stopwords] # remove pontuacoes e stopwords
-    tokens = [token for token in tokens if token != '']
-    stemmer = nltk.stem.RSLPStemmer() 
-    stem_tokens = [stemmer.stem(token) for token in tokens] # stemming
+    tokens = nltk.RegexpTokenizer(r'\w+').tokenize(text) #tokenize
+
+    stemmer = nltk.SnowballStemmer('portuguese')
+    stem_tokens = [stemmer.stem(token.lower()) for token in tokens if token not in stopwords] # lowercase, remove stopwords and stemming
     return stem_tokens
 
-def query_preprocess_thread(queries):
+def query_preprocess(queries):
     terms_used = set()
     for q in queries:
         prepro_q = preprocess(q)
         for t in prepro_q:
             terms_used.add(t)
         next_query.put((q.replace('\n', ''), prepro_q))
-    
-    # acabou de preprocessar todas as consultas, entao libera a thread
-    # global num_threads
-    # mutex_count_threads.acquire()
-    # num_threads -= 1
-    # mutex_count_threads.release()
     return terms_used
 
 def compute_idf(postings):
@@ -74,75 +64,47 @@ def compute_bm25(tf, docid, postings):
     length = docid_to_url[docid][1] # tamanho da pagina
     return (tf * (k1 + 1)/(tf + k1 *(1 - b + b*(length/avg_doc_length))))*idf
 
+def get_min_docid_from_pointers(pointers, query):
+    d = []
+    for i, term in enumerate(query):
+        if pointers[i] < len(postings[term]):
+            d.append(postings[term][pointers[i]][0])
+    if d == []:
+        return None
+    return min(d)
+
 def daat_thread(original, query, index, ranker):
     docs = []
     heapify(docs)
     new_query = []
-    # with open(index, 'r') as ind:
-    docids_count = defaultdict(int)
-    # set_docs = set()
     for term in query:
         if term not in postings:
             continue
-            # ind.seek(int(token_to_offsets[term]))
-            # line = json.loads(ind.readline())
-            # postings[term] = line[term]
-        # print(len(postings[term]))
-        for docid, tf in postings[term]:
-            docids_count[docid] += 1
-            # set_docs.add(docid)
         new_query.append(term)
-    pos = [0 for i in range(len(new_query))]
+    pointers = [0 for i in range(len(new_query))]
     
-    s_docids = {k: v for k, v in sorted(docids_count.items(), key=lambda item: item[1], reverse=True)}
-    # print(s_docids)
-    docids_count.clear()
-    docids = []
-    for d, count in s_docids.items():
-        # print(d, count)
-        # print(count)
-        if count < 0.7*len(new_query):
+    while(1):
+        docid = get_min_docid_from_pointers(pointers, new_query)
+        if docid == None:
             break
-        docids.append(d)
-    
-    s_docids.clear()
-    
-    # print(len(docids))
-    # print(docids)
-    start_daat_time = time.time()
-    print('Start daat: ', original)
-    
-    for target in sorted(docids): # tamanho do indice = numero de documentos
         score = 0
-        # start_for_term = time.time()
         for i, term in enumerate(new_query):
-            p = postings[term][pos[i]:]
-            for j, (docid, tf) in enumerate(p):
-                if docid > target:
-                    pos[i] = j
-                    break
-                if docid == target:
-                    # calculo da metrica
+            if pointers[i] < len(postings[term]):
+                d, tf = postings[term][pointers[i]]
+                if d == docid:
                     if ranker == 'TFIDF':
                         score += compute_tf_idf(tf, postings[term]) 
                     else:
                         score += compute_bm25(tf, docid, postings[term])
-                    pos[i] = j + 1
-                    break
-                
-                # pos[i] += 1
-            # print('Tempo for docs: ', time.time()-start_for_docs)
-        # print('Tempo for termos: ', time.time() - start_for_term)
-        
-        heappush(docs, (-1*score, target))
-    print('Tempo for daat: ', time.time() - start_daat_time)
+                    pointers[i] += 1
+        heappush(docs, (-1*score, docid))
+    
     
     results = []
     for i in range(min(len(docs), 10)):
         (score, docid) = heappop(docs)
         results.append({"URL": docid_to_url[docid][0], "Score": -1*score})
-    # print('Saida: ', score)
-    # print('Saida: ', -1*score)
+
     to_print = {
         "Query": original, 
         "Results": results
@@ -165,7 +127,6 @@ def daat_thread(original, query, index, ranker):
     
 def wait_threads_end():
     while(1):
-        # print(num_threads)
         mutex_count_threads.acquire()
         if num_threads == 0:
             mutex_count_threads.release()
@@ -195,7 +156,7 @@ def queries_scheduler(index, ranker):
             
         # pega a proxima consulta na fila
         query, query_p = next_query.get()
-        # print(query)
+       
         # verifica se pode criar mais uma thread, caso contrário espera 
         global num_threads
         while(1):
@@ -212,7 +173,7 @@ def queries_scheduler(index, ranker):
         t = Thread (target = daat_thread, args = (query, query_p, index, ranker)) 
         t.start()
 
-def get_postings(index):
+def get_postings(index, terms_used):
     global postings
     postings = {}
     with open(index, 'r') as ind:
@@ -227,6 +188,11 @@ def get_postings(index):
 def main(path_queries, index, ranker):
     with open(path_queries, 'r') as f_queries:
         queries = f_queries.readlines()
+
+    global num_queries 
+    num_queries = len(queries)
+    terms_used = query_preprocess(queries)
+    queries.clear()
     
     with open("save_url_to_docid.txt", 'r') as f:
         urls = f.readlines()
@@ -244,9 +210,6 @@ def main(path_queries, index, ranker):
     len_index = len(docid_to_url)
     avg_doc_length /= len_index
 
-    global terms_used
-    terms_used = query_preprocess_thread(queries)
-
     with open("save_token_offset.txt", 'r') as f:
         offsets = f.readlines()
 
@@ -256,14 +219,9 @@ def main(path_queries, index, ranker):
             token_to_offsets[splitted[0]] = int(splitted[1].replace('\n', ''))
     offsets.clear()
 
-    start_postings = time.time()
-    get_postings(index)
-    # print('End get postings: ', time.time()-start_postings)
-
+    get_postings(index, terms_used)
+    terms_used.clear()
     token_to_offsets.clear()
-
-    global num_queries 
-    num_queries = len(queries)
 
     # global num_threads
     # num_threads += 1
